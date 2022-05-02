ae5de41cb97332fb4b3cef753a1a96036e8f8138
Dustin Breese <dbreese@salesforce.com>
2022-05-02 14:51:24 -0600

    Added more playground stuff while I learn
    Mainly started validating some assumptions I had with Classes and Structs (but got interrupted so will have to come back to it).

--- Pages/Classes And Structs.xcplaygroundpage/Contents.swift
+++ Pages/Classes And Structs.xcplaygroundpage/Contents.swift
@@ -1,20 +1,51 @@
 import Foundation
 
-protocol Fighter {
-    var engines: Int { get }
-    var sorties: Int { get set }
-    
-    func launch(fighter: inout Fighter) -> Fighter
-}
-struct XWing : Fighter {
-    var engines: Int
-    var sorties: Int
+// TODO: associatedtype
 
-    func launch(fighter: inout Fighter) -> Fighter {
-        fighter.sorties = fighter.sorties + 1
-        return fighter
+protocol Counter {
+    var count: Int { get set }
+}
+
+code(for: "Structs pass by reference", active: false) {
+    struct DogCounter: Counter {
+        var count: Int
     }
+    
+    let valueFunction = { (counter: Counter) -> Void in
+        //                counter.count += 1 // can't do it -- counter is a let constant
+    }
+    
+    let mutatingFunction = { (counter: inout Counter) -> Void in
+        counter.count += 1
+    }
+    
+    var dc: Counter = DogCounter(count:0)
+    valueFunction(dc)
+    mutatingFunction(&dc) // must be a var
+    mutatingFunction(&dc) // must be a var
+    mutatingFunction(&dc) // must be a var
+    print("counter = \(dc)")
 }
 
+
+// from https://www.youtube.com/watch?v=DvHkeUxiwYY
+protocol Fighter: Equatable { }
+code(for: "Structs referencing Self", active: true) {
+    struct XWing: Fighter { }
+    struct YWing: Fighter { }
+
+    func launchFighter() -> some Fighter {
+        Int.random(in: 0...1) == 0 ? XWing(): XWing()
+    }
+    
+    let fighter1 = launchFighter()
+    let fighter2 = launchFighter()
+    
+    // ERR: == can't be applied to Fighter operands
+    //    if (red5_1 == red5_2) {}
+//    fighter1 == fighter2
+    
+    
+}
 
 
--- Sources/utils.swift
+++ Sources/utils.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 let hr = "---------------------------"
 
+/*
+ Utility method to use in playgrounds that add a header and allow a block to be turned off/on more easily for testing and learning.
+ */
 public func code(for title:String, active:Bool = true, closure: () throws ->()) -> String? {
     guard active else {
         return title // so it shows up in margin
